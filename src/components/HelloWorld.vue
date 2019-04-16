94f538ba84b7736c9f39a5e0e6b7d9bd950bd94e
willhunt <Mars#100>
2019-04-16 19:34:36 -0400

    Testing vuetify cards and d3 chart

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,147 +1,72 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
     </v-layout>
+
+    <v-card height="300">
+      <p>Test #1</p>
+      <div v-for="(data) in data" :key="data.dataId">
+        <p> {{data}} </p>
+      </div>
+    </v-card>
+
+
+    <v-card>Test #2
+      <svg width="500" height="270">
+        <g style="transform: translate(0, 10px)">
+          <path :d="line" />
+        </g>
+      </svg>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+  import * as d3 from 'd3';
 
-      ]
-    })
+  export default {
+    data() {
+      return {
+        data: [2,1,5,2,3,0,1],
+        line: '',
+      }
+    },
+
+    mounted() {
+      this.calculatePath();
+    },
+
+    methods: {
+      getScales() {
+        const x = d3.scaleTime().range([0, 600]);
+        const y = d3.scaleLinear().range([210, 0]);
+        d3.axisLeft().scale(x);
+        d3.axisBottom().scale(y);
+        x.domain(d3.extent(this.data, (d, i) => i));
+        y.domain([0, d3.max(this.data, d => d)]);
+        return { x, y };
+      },
+      calculatePath() {
+        const scale = this.getScales();
+        const path = d3.line()
+                .x((d, i) => scale.x(i))
+                .y(d => scale.y(d));
+        this.line = path(this.data);
+      },
+    },
   }
+
+
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+  svg
+    margin: 25px
+  path
+    fill: none
+    stroke: #76BF8A
+    stroke-width: 3px
 </style>
